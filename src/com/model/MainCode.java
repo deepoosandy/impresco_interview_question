3e7035522ccd0bfccec20894953bdcc7397223f8
deepoosandy <sandeepgeu@gmail.com>
2019-03-10 00:47:33 +0530

    it checks soucre and destinations are connected or not

---
+++ src/com/model/MainCode.java
@@ -0,0 +1,89 @@
+package com.model;
+
+ 
+
+import java.util.HashSet;
+
+import java.util.Set;
+
+import java.util.concurrent.CopyOnWriteArraySet;
+
+ 
+
+public class MainCode {
+
+       private static Set numbers = new HashSet();
+
+       private static CopyOnWriteArraySet<Object> objts = new CopyOnWriteArraySet<>();
+
+ 
+
+       public static void main(String[] args) {
+
+ 
+
+              Object obj = new Object[] { 10, new Object[] { 2, 3 }, 4, 5, new Object[] { 6, 7, 8 },
+
+                           new Object[] { 9, 10, new Object[] { 12,new Object[]{100}, 13 }, 15, 15 }};
+
+              check(obj);
+
+              System.out.println(numbers);
+
+       }
+
+ 
+
+       private static void check(Object obj) {
+
+              Object []ii = (Object[]) obj;
+
+              boolean flag = false;
+
+              for (int i = 0; i < ii.length; i++) {
+
+                     boolean t = ii[i] instanceof Number;
+
+                     if (!t) {
+
+                           flag = true;
+
+                     }
+
+ 
+
+              }
+
+ 
+
+              if (flag) {
+
+                     for (int j = 0; j < ii.length; j++) {
+
+                           if (ii[j] instanceof Number) {
+
+                                  numbers.add(ii[j]);
+
+                           } else {
+
+                                  check(ii[j]);
+
+ 
+
+                           }
+
+                     }
+
+ 
+
+             
+
+ 
+
+              }
+
+ 
+
+       }
+
+}
